f60865b37437485678b22bce2a785be94569ed41
Christoph Guttandin <chrisguttandin@media-codings.com>
2016-02-12 18:07:37 +0100

    test against the latest version of OS X

--- config/karma/unit.js
+++ config/karma/unit.js
@@ -44,13 +44,13 @@ module.exports = function (config) {
             ChromeCanarySauceLabs: {
                 base: 'SauceLabs',
                 browserName: 'chrome',
-                platform: 'OS X 10.10',
+                platform: 'OS X 10.11',
                 version: 'dev'
             },
             FirefoxDeveloperSauceLabs: {
                 base: 'SauceLabs',
                 browserName: 'firefox',
-                platform: 'OS X 10.10',
+                platform: 'OS X 10.11',
                 version: 'dev'
             }
         };
