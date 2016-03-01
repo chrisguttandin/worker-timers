4e952cf9a0e82983188a8d8a43876639d6d99967
Christoph Guttandin <chrisguttandin@media-codings.com>
2016-03-01 13:33:43 +0100

    run test in current versions of Chrome & Firefox, too

--- config/karma/unit.js
+++ config/karma/unit.js
@@ -35,7 +35,9 @@ module.exports = function (config) {
     if (process.env.TRAVIS) {
         configuration.browsers = [
             'ChromeCanarySauceLabs',
-            'FirefoxDeveloperSauceLabs'
+            'ChromeSauceLabs',
+            'FirefoxDeveloperSauceLabs',
+            'FirefoxSauceLabs'
         ];
 
         configuration.captureTimeout = 120000;
@@ -47,18 +49,30 @@ module.exports = function (config) {
                 platform: 'OS X 10.11',
                 version: 'dev'
             },
+            ChromeSauceLabs: {
+                base: 'SauceLabs',
+                browserName: 'chrome',
+                platform: 'OS X 10.11'
+            },
             FirefoxDeveloperSauceLabs: {
                 base: 'SauceLabs',
                 browserName: 'firefox',
                 platform: 'OS X 10.11',
                 version: 'dev'
+            },
+            FirefoxSauceLabs: {
+                base: 'SauceLabs',
+                browserName: 'firefox',
+                platform: 'OS X 10.11'
             }
         };
 
         configuration.tunnelIdentifier = process.env.TRAVIS_JOB_NUMBER;
     } else {
         configuration.browsers = [
+            'Chrome',
             'ChromeCanary',
+            'Firefox',
             'FirefoxDeveloper'
         ];
     }
