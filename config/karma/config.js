5764db832a1e0c77fc914d20229bc6f3e1204aaa
chrisguttandin <chrisguttandin@media-codings.com>
2017-07-31 02:01:44 +0200

    chore(config): update Karma config

--- config/karma/config.js
+++ config/karma/config.js
@@ -74,8 +74,8 @@ module.exports = (config) => {
         config.set({
 
             browsers: [
-                'Chrome',
-                'ChromeCanary',
+                'ChromeHeadless',
+                'ChromeCanaryHeadless',
                 'Firefox',
                 'FirefoxDeveloper',
                 'Safari'
