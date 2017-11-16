aca9b2b2898e62294e0b53faa419b13cd59ddc01
Christoph Guttandin <chrisguttandin@media-codings.com>
2017-11-16 15:55:48 +0100

    specify charset for blob

--- src/module.ts
+++ src/module.ts
@@ -1,7 +1,7 @@
 import { load } from 'worker-timers-broker';
 import { worker } from './worker/worker';
 
-const blob: Blob = new Blob([ worker ], { type: 'application/javascript' });
+const blob: Blob = new Blob([ worker ], { type: 'application/javascript; charset=utf-8' });
 
 const url: string = URL.createObjectURL(blob);
 
