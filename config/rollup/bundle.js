4c23899256745cf7240e196f64f3a558e74ec1ba
Christoph Guttandin <chrisguttandin@media-codings.com>
2018-10-05 20:13:42 +0200

    build(config): update rollup config

--- config/rollup/bundle.js
+++ config/rollup/bundle.js
@@ -15,62 +15,47 @@ if (result === null) {
 const workerString = result[1];
 const memoryFileSystem = new MemoryFileSystem();
 
-let transpiledWorkerString = null;
+export default new Promise((resolve, reject) => {
+    const compiler = webpack(webpackConfig);
 
-export default {
-    input: 'build/es2015/module.js',
-    output: {
-        file: 'build/es5/bundle.js',
-        format: 'umd',
-        name: 'workerTimers'
-    },
-    plugins: [
-        {
-            buildStart () {
-                return new Promise((resolve, reject) => {
-                    const compiler = webpack(webpackConfig);
+    compiler.outputFileSystem = memoryFileSystem;
+    compiler.run((err, stats) => {
+        if (stats.hasErrors() || stats.hasWarnings()) {
+            reject(new Error(stats.toString({ errorDetails: true, warnings: true })));
+        }
 
-                    compiler.outputFileSystem = memoryFileSystem;
-                    compiler.run((err, stats) => {
-                        if (stats.hasErrors() || stats.hasWarnings()) {
-                            reject(new Error(stats.toString({ errorDetails: true, warnings: true })));
-                        }
-
-                        transpiledWorkerString = memoryFileSystem.readFileSync('/worker.js', 'utf-8');
-
-                        resolve();
-                    });
-                });
-            }
-        },
-        replace({
-            delimiters: [ '`', '`' ],
-            include: 'build/es2015/worker/worker.js',
-            values: {
-                [ workerString ]: () => {
-                    if (transpiledWorkerString === null) {
-                        throw new Error('The worker should be transpiled by now.');
-                    }
-
-                    return `\`${ transpiledWorkerString }\``;
-                }
-            }
-        }),
-        babel({
-            exclude: 'node_modules/**',
+        resolve({
+            input: 'build/es2015/module.js',
+            output: {
+                file: 'build/es5/bundle.js',
+                format: 'umd',
+                name: 'workerTimers'
+            },
             plugins: [
-                '@babel/plugin-external-helpers',
-                '@babel/plugin-transform-runtime'
-            ],
-            presets: [
-                [
-                    '@babel/preset-env',
-                    {
-                        modules: false
+                replace({
+                    delimiters: [ '`', '`' ],
+                    include: 'build/es2015/worker/worker.js',
+                    values: {
+                        [ workerString ]: `\`${ memoryFileSystem.readFileSync('/worker.js', 'utf-8') }\``
                     }
-                ]
-            ],
-            runtimeHelpers: true
-        })
-    ]
-};
+                }),
+                babel({
+                    exclude: 'node_modules/**',
+                    plugins: [
+                        '@babel/plugin-external-helpers',
+                        '@babel/plugin-transform-runtime'
+                    ],
+                    presets: [
+                        [
+                            '@babel/preset-env',
+                            {
+                                modules: false
+                            }
+                        ]
+                    ],
+                    runtimeHelpers: true
+                })
+            ]
+        });
+    });
+});
