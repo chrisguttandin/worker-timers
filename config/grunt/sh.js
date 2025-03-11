11be9f699b611ad2911028fb94b772f18d2eaf3c
Christoph Guttandin <chrisguttandin@media-codings.com>
2025-03-11 13:52:03 +0100

    build(config): remove linting tasks

--- config/grunt/sh.js
+++ config/grunt/sh.js
@@ -5,15 +5,6 @@ module.exports = (grunt) => {
         'build': {
             cmd: 'npm run build'
         },
-        'lint-config': {
-            cmd: 'npm run lint:config'
-        },
-        'lint-src': {
-            cmd: 'npm run lint:src'
-        },
-        'lint-test': {
-            cmd: 'npm run lint:test'
-        },
         'test-integration-browser': {
             cmd: `karma start config/karma/config-integration.js ${continuous ? '--concurrency Infinity' : '--single-run'}`
         },
